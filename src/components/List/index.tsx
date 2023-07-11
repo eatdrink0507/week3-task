1ac4cc0001a017d7234850c8dd69fd250744837c
FE41PIW <eatdrink0507@gmail.com>
2023-07-12 00:08:34 +0900

    Feat List Component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,8 +1,24 @@
-export default function List() {
+export default function List({ data }: Props) {
   return (
     <div>
       <h3>issue title</h3>
+      <h3>{data.title}</h3>
       <span>작성자 name, 작성일 2022년 3월</span>
+      <span>
+        작성자: {data.user.login}, 작성일: {data.created_at}
+      </span>
     </div>
   );
 }
+
+type Props = {
+  data: {
+    number: number;
+    title: string;
+    user: {
+      login: string;
+    };
+    created_at: string;
+    comments: number;
+  };
+};
