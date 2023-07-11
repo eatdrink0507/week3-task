00889c58e48e32a8b0e7d4d2c9f947a3a7f08d0e
FE41PIW <eatdrink0507@gmail.com>
2023-07-12 02:34:31 +0900

    Feat Data Fetching Function

--- src/getIssues.tsx
+++ src/getIssues.tsx
@@ -1,1 +1,17 @@
-export default function getIssues() {}
+import axios from 'axios';
+
+export default function getIssues(
+  per: number,
+  pagenum: number,
+  setData: Function,
+) {
+  axios
+    .get(`${process.env.REACT_APP_URL}&per_page=${per}&page=${pagenum}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+      },
+    })
+    .then(res => {
+      setData(res.data);
+    });
+}
