2e48ecd509948bc10947bb30526a2ef8cfd5647c
FE41PIW <eatdrink0507@gmail.com>
2023-07-12 17:05:24 +0900

    Feat Header에 이동 기능 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  return <header>facebook / react</header>;
+  const navigate = useNavigate();
+  return (
+    <header css={HeaderStyle}>
+      <h1 onClick={() => navigate('/')}>facebook / react</h1>
+    </header>
+  );
 };
 
 export default Header;
+
+const HeaderStyle = css`
+  height: 50px;
+  border-bottom: dotted 1px black;
+  margin-bottom: 20px;
+  text-align: center;
+`;
