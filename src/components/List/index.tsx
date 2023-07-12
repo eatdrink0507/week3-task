881a7a4f8f0807d69296e93fe4c7b805a5c9d527
FE41PIW <eatdrink0507@gmail.com>
2023-07-12 15:13:42 +0900

    Feat ListComponent Update

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,10 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-export default function List({ data }: Props) {
+export default function List({ data, open }: Props) {
   return (
     <div className="div" css={listStyle}>
-      <h3>{data.title}</h3>
+      <h3 onClick={() => open(true)}>
+        #{data.number} {data.title}
+      </h3>
       <span>
         작성자: {data.user.login}, 작성일: {data.created_at}
       </span>
@@ -14,6 +16,7 @@ export default function List({ data }: Props) {
 }
 
 type Props = {
+  open: Function;
   data: {
     number: number;
     title: string;
@@ -24,4 +27,9 @@ type Props = {
     comments: number;
   };
 };
-const listStyle = css({ border: 'solid 1px black', color: 'blue' });
+const listStyle = css`
+  border: solid 1px black;
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+`;
