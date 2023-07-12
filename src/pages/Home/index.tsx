9ef3d5614e620206f5cda9a11f13529f1fbd7553
FE41PIW <eatdrink0507@gmail.com>
2023-07-12 18:19:12 +0900

    Feat Home에 로딩 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,17 @@ import { css } from '@emotion/react';
 import { useState, useEffect } from 'react';
 import getIssues from '../../getIssues';
 import List from '../../components/List';
+import Loading from '../Loading';
 
 export default function Home() {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     getIssues(10, 1, setData);
   }, []);
   return (
     <div css={style}>
+      {data.length > 0 || <Loading />}
       {data.map((e, i: number) => {
         return <List key={i} index={i} data={e} />;
       })}
